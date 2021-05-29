e680ffc13aee4cd1c1f7f0be2a14bd516c568ace
EdonJung <2donny@naver.com>
2021-05-29 20:37:53 +0900

    photo resolver

--- src/photos/editPhoto/editPhoto.resolver.ts
+++ src/photos/editPhoto/editPhoto.resolver.ts
@@ -1,29 +1,37 @@
 import { Resolvers } from '../../types';
 import { protectedResolver } from '../../users/users.utils';
+import { processHashtags } from '../photos.utils';
 
 const resolvers: Resolvers = {
   Mutation: {
     editPhoto: protectedResolver(
       async (_, { id, caption }, { client, loggedInUser }) => {
-        const ok = client.photo.findUnique({ where: { id } });
-        if (!ok) {
+        const oldPhoto = await client.photo.findUnique({
+          where: { id },
+          include: {
+              hashtags: {
+                select: {
+                  hashtag: true
+                }
+              }
+          },
+        });
+        if (!oldPhoto) {
           return {
             ok: false,
             error: "Can't found photo id.",
           };
         }
-        const hashtagArr = caption.match(/#[\w]+/g);
-
-        const prevHashtagArr = await client.photo
-          .findUnique({ where: { id } })
-          .hashtags();
-
-        console.log(prevHashtagArr);
-
+        const hashtagObj = processHashtags(caption);
+        
         await client.photo.update({
           where: { id },
           data: {
             caption,
+            hashtags: {
+              disconnect: oldPhoto.hashtags,
+              ...(hashtagObj && { connectOrCreate: hashtagObj}),
+            },
           },
         });
 
--- src/photos/uploadPhoto/uploadPhoto.resolver.ts
+++ src/photos/uploadPhoto/uploadPhoto.resolver.ts
@@ -1,21 +1,22 @@
 import { Resolvers } from '../../types';
 import { protectedResolver } from '../../users/users.utils';
-import { createWriteStream } from 'fs';
+import { processHashtags } from '../photos.utils';
 
 const resolvers: Resolvers = {
   Mutation: {
     uploadPhoto: protectedResolver(
       async (_, { file, caption }, { client, loggedInUser }) => {
-        let hashtagsObj = [];
+        let hashtagsObj = null;
         if (caption) {
-          const hashtags = caption.match(/#[\w]+/g);
-          console.log(hashtags);
-          hashtagsObj = hashtags.map((hashtag) => ({
-            where: { hashtag },
-            create: { hashtag },
-          }));
+          hashtagsObj = processHashtags(caption);
         }
 
+        const user = await client.user.findMany({
+          where: {
+            
+          }
+        })
+        
         return await client.photo.create({
           data: {
             file,
@@ -25,16 +26,14 @@ const resolvers: Resolvers = {
                 id: loggedInUser.id,
               },
             },
-            ...(hashtagsObj.length !== 0 && { hashtags: { connectOrCreate: hashtagsObj } }),
+            ...(hashtagsObj && {
+              hashtags: { 
+                connectOrCreate: hashtagsObj 
+              },
+            }),
           },
         });
 
-        // const { filename, createReadStream } = await file;
-        // const uniquePhotoName = `${loggedInUser.id}-${Date.now()}-${filename}`;
-        // const readStream = createReadStream();
-        // const writeStream = createWriteStream(process.cwd() + '/uploads/' + uniquePhotoName);
-        // readStream.pipe(writeStream);
-        // const photoUrl = `http://localhost:4000/static/${uniquePhotoName}`;
       },
     ),
   },
