df3ee1241c2446857fa9d7838a9911506eaf1e96
EdonJung <2donny@naver.com>
2021-05-30 17:49:33 +0900

    Comment model

--- prisma/migrations/20210527092423_hashtag/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `hashtag` to the `Hashtag` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Hashtag" ADD COLUMN     "hashtag" TEXT NOT NULL;
