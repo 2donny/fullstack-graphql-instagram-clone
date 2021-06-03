1de55ea95a65d74561d55dc2c96658e5ca4f8229
EdonJung <2donny@naver.com>
2021-06-03 16:29:23 +0900

    app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,31 @@
 import { useReactiveVar } from '@apollo/client';
-import React from 'react';
+import { ThemeProvider } from 'styled-components';
 import { Route, Switch } from 'react-router-dom';
-import { isLoggedInVar } from './Apollo';
+import { isLoggedInVar, darkModeVar } from './Apollo';
+import { darkTheme, lightTheme, GlobalStyles } from './styles/styles';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import NotFound from './pages/NotFound';
+import SignUp from './pages/SignUp';
 
 function App() {
   const isLoggedIn = useReactiveVar(isLoggedInVar);
+  const isDarkMode = useReactiveVar(darkModeVar);
 
   return (
-    <>
+    <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
+      <GlobalStyles />
       <Switch>
         <Route path="/" exact>
           {isLoggedIn ? <Home /> : <Login />}
         </Route>
+        <Route path="/sign-up" component={SignUp}/>
 
         <Route>
           <NotFound />
         </Route>
       </Switch>
-    </>
+    </ThemeProvider>
   );
 }
 
