dbd0205fe262ca69df6b4ecafc32032669fbf896
EdonJung <2donny@naver.com>
2021-06-07 13:46:34 +0900

    all types

--- client/src/shared/types.d.ts
+++ client/src/shared/types.d.ts
@@ -2,5 +2,53 @@ export interface MutationResponse {
   ok: boolean;
   error?: string;
   token?: string;
-  [key: string?]: {};
+  [key: ?string]: {};
+}
+
+export interface UserTypes {
+  id?: string;
+  name?: string;
+  username?: string;
+  email?: string;
+  bio?: string;
+  avatar?: string;
+  followers?: [UserTypes];
+  followings?: [UserTypes];
+  photos?: [PhotoTypes];
+  totalFollowings?: number;
+  totalFollowers?: number;
+  isMe?: boolean;
+  isFollowing?: boolean;
+  createdAt?: string;
+  updatedAt?: string;
+}
+
+export interface PhotoTypes {
+  id?: number;
+  user?: UserTypes;
+  file?: string;
+  caption?: string;
+  hashtags?: [HashtagTypes];
+  createdAt?: string;
+  updatedAt?: string;
+  isMine?: boolean;
+  likes?: number;
+  isLiked?: boolean;
+  comments?: number;
+}
+
+export interface LikeTypes {
+  id?: number;
+  photo?: PhotoTypes;
+  createdAt?: string;
+  updatedAt?: string;
+}
+
+export interface HashtagTypes {
+  id?: number;
+  hashtag?: string;
+  photos?: [PhotoTypes];
+  totalPhotos?: number;
+  createdAt?: string;
+  updatedAt?: string;
 }
