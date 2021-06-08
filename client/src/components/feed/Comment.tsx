1be52fbd6d90fcd031859c66064cfefc14f4b658
EdonJung <2donny@naver.com>
2021-06-08 13:12:08 +0900

    parsing Hashtags

--- client/src/components/feed/Comment.tsx
+++ client/src/components/feed/Comment.tsx
@@ -1,27 +1,30 @@
+import React from 'react';
 import styled from 'styled-components';
-import sanitizeHtml from 'sanitize-html';
 import { FatText } from '../shared';
+import { Link } from 'react-router-dom';
 
 interface Props {
   username: string;
   payload: string;
 }
 
-export default function Comment({ username, payload }: Props) {
-  const cleanedPayload = sanitizeHtml(
-    payload.replace(/#[ㄱ-ㅎㅏ-ㅣ가-힣]+/g, '<mark>$&</mark>'), {
-        allowedTags: ['mark']
-    }
-  );
+const hashtagsRegex = /#[ㄱ-ㅎㅏ-ㅣ가-힣]+/g;
 
+export default function Comment({ username, payload }: Props) {
   return (
     <CommentContainer>
       <FatText>{username}</FatText>
-      <CommentCaption
-        dangerouslySetInnerHTML={{
-          __html: cleanedPayload,
-        }}
-      />
+      <CommentCaption>
+        {payload.split(' ').map((word, index) =>
+          hashtagsRegex.test(word) ? (
+            <React.Fragment key={index}>
+              <Link to={`/hashtags/${word}`}>{word}</Link>{' '}
+            </React.Fragment>
+          ) : (
+            <React.Fragment key={index}>{word} </React.Fragment>
+          ),
+        )}
+      </CommentCaption>
     </CommentContainer>
   );
 }
@@ -31,7 +34,7 @@ export const CommentContainer = styled.div`
 `;
 const CommentCaption = styled.span`
   margin-left: 5px;
-  mark {
+  a {
     color: ${(props) => props.theme.accent};
     background-color: inherit;
     cursor: pointer;
