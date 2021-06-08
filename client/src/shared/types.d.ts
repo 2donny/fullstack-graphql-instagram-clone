ab0ee67bd27aadb50ea436a44b2a2593950ebbe0
EdonJung <2donny@naver.com>
2021-06-08 13:14:56 +0900

    types mapping with GraphQL query

--- client/src/shared/types.d.ts
+++ client/src/shared/types.d.ts
@@ -1,65 +1,76 @@
+export type Array<T> = [T];
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: number;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+  Upload: any;
+};
+
 export interface MutationResponse {
-  ok: boolean;
-  error?: string;
-  token?: string;
-  [key: ?string]: {};
+  ok: Scalars['Boolean'];
+  error: Scalars['String'];
+  token: Scalars['String'];
+  id: Scalars['Int'];
 }
 
 export interface UserTypes {
-  id?: string;
-  name?: string;
-  username?: string;
-  email?: string;
-  bio?: string;
-  avatar?: string;
-  followers?: [UserTypes];
-  followings?: [UserTypes];
-  photos?: [PhotoTypes];
-  totalFollowings?: number;
-  totalFollowers?: number;
-  isMe?: boolean;
-  isFollowing?: boolean;
-  createdAt?: string;
-  updatedAt?: string;
+  id: Scalars['String'];
+  name: Scalars['String'];
+  username: Scalars['String'];
+  email: Scalars['String'];
+  bio: Scalars['String'];
+  avatar: Scalars['String'];
+  followers: [UserTypes];
+  followings: [UserTypes];
+  photos: [PhotoTypes];
+  totalFollowings: Scalars['Int'];
+  totalFollowers: Scalars['Int'];
+  isMe: Scalars['boolean'];
+  isFollowing: Scalars['boolean'];
+  createdAt: Scalars['String'];
+  updatedAt: Scalars['String'];
 }
 
 export interface PhotoTypes {
-  id?: number;
-  user?: UserTypes;
-  file?: string;
-  caption?: string;
-  hashtags?: [HashtagTypes];
-  createdAt?: string;
-  updatedAt?: string;
-  isMine?: boolean;
-  likes?: number;
-  isLiked?: boolean;
-  commentNumber?: number;
-  comments?: [CommentTypes]
+  id: Scalars['ID'];
+  user: UserTypes;
+  file: Scalars['String'];
+  caption: Scalars['String'];
+  hashtags: [HashtagTypes];
+  createdAt: Scalars['String'];
+  updatedAt: Scalars['String'];
+  isMine: Scalars['boolean'];
+  likes: Scalars['Int'];
+  isLiked: Scalars['boolean'];
+  commentNumber: Scalars['Int'];
+  comments: [CommentTypes];
 }
 
 export interface LikeTypes {
-  id?: number;
-  photo?: PhotoTypes;
-  createdAt?: string;
-  updatedAt?: string;
+  id: Scalars['Int'];
+  photo: PhotoTypes;
+  createdAt: Scalars['String'];
+  updatedAt: Scalars['String'];
 }
 
 export interface HashtagTypes {
-  id?: number;
-  hashtag?: string;
-  photos?: [PhotoTypes];
-  totalPhotos?: number;
-  createdAt?: string;
-  updatedAt?: string;
+  id: Scalars['Int'];
+  hashtag: Scalars['String'];
+  photos: [PhotoTypes];
+  totalPhotos: Scalars['Int'];
+  createdAt: Scalars['String'];
+  updatedAt: Scalars['String'];
 }
 
 export interface CommentTypes {
-  id?: number
-  user?: UserTypes
-  photo?: PhotoTypes
-  payload?: string
-  isMine?: boolean
-  createdAt?: string
-  updatedAt?: string
+  id: Scalars['Int'];
+  user: UserTypes;
+  photo: PhotoTypes;
+  payload: Scalars['String'];
+  isMine: Scalars['boolean'];
+  createdAt: Scalars['String'];
+  updatedAt: Scalars['String'];
 }
