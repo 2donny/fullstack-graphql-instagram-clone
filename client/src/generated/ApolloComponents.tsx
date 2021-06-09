5c5cca232473bead34b3d4e7973266c5d314c980
EdonJung <2donny@naver.com>
2021-06-09 17:39:50 +0900

    Apollo hooks

--- client/src/generated/ApolloComponents.tsx
+++ client/src/generated/ApolloComponents.tsx
@@ -4,7 +4,12 @@ import {
   useMutation,
   useQuery,
 } from '@apollo/client';
-import { MutationResponse, PhotoTypes } from '../shared/types';
+import {
+  COMMENT_FRAGMENT,
+  PHOTO_FRAGMENT,
+  USER_FRAGMENT,
+} from '../graphql/fragments';
+import { MutationResponse, PhotoTypes, UserTypes } from '../shared/types';
 import { Scalars } from '../shared/types';
 
 type Maybe<T> = T | null;
@@ -45,6 +50,125 @@ export type createCommentMutationVariables = {
   photoId: Scalars['Int'];
   payload: Scalars['String'];
 };
+
+export type deleteCommentMutation = {
+  deleteComment: MutationResponse;
+};
+export type deleteCommentVariables = {
+  commentId: Scalars['Int'];
+};
+
+export type seeProfileQuery = {
+  seeProfile: UserTypes;
+};
+export type seeProfileVariables = {
+  username: Scalars['String'];
+  page: Scalars['Int'];
+};
+
+export type followUserMutation = {
+  followUser: MutationResponse;
+};
+export type followUserVariables = {
+  username: Scalars['String'];
+};
+
+export const followUserDocument = gql`
+  mutation followUser($username: String!) {
+    followUser(username: $username) {
+      ok
+      error
+    }
+  }
+`;
+export function useFollowUserMutation(
+  baseOptions?: MutationHookOptions<followUserMutation, followUserVariables>,
+) {
+  const [followUser] = useMutation<followUserMutation, followUserVariables>(
+    followUserDocument,
+    baseOptions,
+  );
+  return followUser;
+}
+
+
+export type unfollowUserMutation = {
+  unfollowUser: MutationResponse;
+};
+export type unfollowUserVariables = {
+  username: Scalars['String'];
+};
+
+export const unfollowUserDocument = gql`
+  mutation unfollowUser($username: String!) {
+    unfollowUser(username: $username) {
+      ok
+      error
+    }
+  }
+`;
+export function useUnfollowUserMutation(
+  baseOptions?: MutationHookOptions<
+    unfollowUserMutation,
+    unfollowUserVariables
+  >,
+) {
+  const [unfollowUser, { data }] = useMutation<unfollowUserMutation, unfollowUserVariables>(
+    unfollowUserDocument,
+    baseOptions,
+  );
+  console.log(data);
+  return unfollowUser;
+}
+
+export const seeProfileDocument = gql`
+  ${PHOTO_FRAGMENT}
+  ${USER_FRAGMENT}
+  query seeProfile($username: String!, $page: Int!) {
+    seeProfile(username: $username) {
+      ...UserFragment
+      name
+      bio
+      isFollowing
+      totalFollowings
+      totalFollowers
+      photos(page: $page) {
+        ...PhotoFragment
+      }
+    }
+  }
+`;
+export function useSeeProfileQuery(
+  baseOptions?: MutationHookOptions<seeProfileQuery, seeProfileVariables>,
+) {
+  const { loading, data } = useQuery<seeProfileQuery, seeProfileVariables>(
+    seeProfileDocument,
+    baseOptions,
+  );
+  return { loading, data };
+}
+
+export const deleteCommentDocument = gql`
+  mutation deleteComment($commentId: Int!) {
+    deleteComment(commentId: $commentId) {
+      ok
+      error
+    }
+  }
+`;
+export function useDeleteCommentMutation(
+  baseOptions?: MutationHookOptions<
+    deleteCommentMutation,
+    deleteCommentVariables
+  >,
+) {
+  const [deleteComment, { loading }] = useMutation<
+    deleteCommentMutation,
+    deleteCommentVariables
+  >(deleteCommentDocument, baseOptions);
+
+  return { deleteComment, loading };
+}
 
 export const createCommentDocument = gql`
   mutation createComment($photoId: Int!, $payload: String!) {
@@ -91,31 +215,24 @@ export function useToggleLikePhotoMutation(
 }
 
 export const seeFeedDocument = gql`
+  ${PHOTO_FRAGMENT}
+  ${COMMENT_FRAGMENT}
+  ${USER_FRAGMENT}
   query seeFeed($page: Int!) {
     seeFeed(page: $page) {
-      id
+      ...PhotoFragment
       user {
         username
         avatar
       }
-      file
       caption
-      likes
-      commentNumber
       createdAt
       isMine
-      isLiked
       comments {
-        id
+        ...CommentFragment
         user {
-          username
-          avatar
-          isMe
-          createdAt
+          ...UserFragment
         }
-        payload
-        isMine
-        createdAt
       }
     }
   }
