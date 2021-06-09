c8306270a19ded366a6b70f84532353238761d56
EdonJung <2donny@naver.com>
2021-06-09 17:39:32 +0900

    apollo fragment

---
+++ client/src/graphql/fragments.tsx
@@ -0,0 +1,29 @@
+import { gql } from '@apollo/client';
+
+export const PHOTO_FRAGMENT = gql`
+    fragment PhotoFragment on Photo {
+        id
+        file
+        likes
+        commentNumber
+        isLiked
+    }
+`;
+
+export const COMMENT_FRAGMENT = gql`
+    fragment CommentFragment on Comment {
+        id
+        payload
+        isMine
+        createdAt
+    }
+`;
+
+export const USER_FRAGMENT = gql`
+    fragment UserFragment on User {
+        id
+        username
+        avatar
+        isMe
+    }
+`;
